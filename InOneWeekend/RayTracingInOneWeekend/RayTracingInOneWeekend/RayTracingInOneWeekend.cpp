c7150600d9a875a34cc400ef216a2f5a29b8a6e9
毛善辉 <>
2019-11-01 22:11:21 +0800

    8.DIffuse Materials

--- InOneWeekend/RayTracingInOneWeekend/RayTracingInOneWeekend/RayTracingInOneWeekend.cpp
+++ InOneWeekend/RayTracingInOneWeekend/RayTracingInOneWeekend/RayTracingInOneWeekend.cpp
@@ -12,16 +12,29 @@
 
 using namespace std;
 
-float hit_sphere(const vec3& center, float radius, const ray&r) {
+vec3 random_in_unit_sphere() 
+{
+	vec3 p;
+	do 
+	{
+		p = 2.0 * vec3(random_double(), random_double(), random_double()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+
+float hit_sphere(const vec3& center, float radius, const ray&r) 
+{
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius * radius;
 	float discriminant = b*b - 4*a*c;
-	if (discriminant < 0) {
+	if (discriminant < 0) 
+	{
 		return -1.0;
 	}
-	else {
+	else 
+	{
 		return (-b - sqrt(discriminant)) / (2.0 * a);
 	}
 	return (discriminant > 0); 
@@ -30,10 +43,13 @@ float hit_sphere(const vec3& center, float radius, const ray&r) {
 vec3 color(const ray& r, hittable *world) 
 {
 	hit_record rec;
-	if(world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+	if(world->hit(r, 0.001, FLT_MAX, rec)) 
+	{
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world); // 递归地去计算反射光线，0.5衰减 
 	}
-	else {
+	else 
+	{
 		vec3 unit_direction = unit_vector(r.direction());
 		float t = 0.5 * (unit_direction.y() + 1.0);
 		return (1.0-t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
@@ -45,8 +61,8 @@ int main()
 	ofstream outfile;
 	outfile.open("./outputImage.ppm", ios::out);
 
-	int nx = 200;
-	int ny = 100;
+	int nx = 800;
+	int ny = 400;
 	int ns = 100;
 	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 	vec3 lower_left_corner(-2.0, -1.0, -1.0);
@@ -72,6 +88,7 @@ int main()
 				col += color(r, world);
 			}
 			col /= float(ns);
+			col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
